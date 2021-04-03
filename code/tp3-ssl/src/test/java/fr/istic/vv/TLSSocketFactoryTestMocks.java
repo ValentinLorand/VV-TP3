7de32943b46130080558e8c3b001d0e9d75271a2
Lorand Valentin <valentin.lorand@etudiant.univ-rennes1.fr>
2021-04-03 16:39:35 +0200

    Mocks terminate

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -1,13 +1,12 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,10 +23,13 @@ public class TLSSocketFactoryTestMocks {
     //Setup du mock
     when(mockedSocket.getSupportedProtocols()).thenReturn(null);
     when(mockedSocket.getEnabledProtocols()).thenReturn(null);
-    verify(mockedSocket, never()).setEnabledProtocols(new String[0]);
+    
     //Utilisation du mock
-    mockedSocket.setEnabledProtocols(new String[0]); //=> doit faire echouer le test
+    // mockedSocket.setEnabledProtocols(new String[0]); //=> fait echouer le test
     f.prepareSocket(mockedSocket);
+
+    //Verifications
+    Mockito.verify(mockedSocket, Mockito.times(0)).setEnabledProtocols(any(String[].class));
   }
   
   @Test
@@ -37,11 +39,15 @@ public class TLSSocketFactoryTestMocks {
     SSLSocket mockedSocket = mock(SSLSocket.class);
     //Setup du mock
     when(mockedSocket.getSupportedProtocols()).thenReturn(shuffle(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"}));
-    when(mockedSocket.getEnabledProtocols()).thenReturn(new String[]{"SSLv3", "TLSv1"});
-    // String[] result = new String[0];
-    // Mockito.doCallRealMethod(assertTrue(Arrays.equals(result, new String[] {"TLSv1.2", "TLSv1.1", "TLSv1", "SSLv3" }))).when(mockedSocket).setEnabledProtocols(result);
+    when(mockedSocket.getEnabledProtocols()).thenReturn(shuffle(new String[]{"SSLv3", "TLSv1"}));
+
     //Utilisation du mock
     f.prepareSocket(mockedSocket);
+
+    //Verifications
+    final ArgumentCaptor<String[]> captor = ArgumentCaptor.forClass(String[].class);
+    Mockito.verify(mockedSocket, Mockito.times(1)).setEnabledProtocols(captor.capture());
+    assertTrue(Arrays.equals(captor.getValue(), new String[] {"TLSv1.2", "TLSv1.1", "TLSv1", "SSLv3" }));
   }
   
   private String[] shuffle(String[] in) { 
